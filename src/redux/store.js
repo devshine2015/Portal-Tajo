35691383878cf5c3d5e941c02b175ce7a25ccc77
nikita <nikita.afanasev2020@gmail.com>
2016-06-06 20:49:13 +0700

    initial

---
+++ src/redux/store.js
@@ -0,0 +1,52 @@
+/**
+ * Create the store with asynchronously loaded reducers
+ */
+
+import { createStore, applyMiddleware, compose } from 'redux';
+import { fromJS } from 'immutable';
+import thunk from 'redux-thunk';
+import createReducer from './reducers';
+
+const devtools = window.devToolsExtension || (() => noop => noop);
+const isDev = process.env.NODE_ENV !== 'production';
+
+export default function configureStore(initialState = {}) {
+
+  const enhancers = [
+    devtools(),
+  ];
+  const middlewares = [
+    thunk
+  ];
+
+  if (isDev) {
+    const createLogger = require('redux-logger');
+    const logger = createLogger({
+      predicate: isDev,
+      collapsed: true,
+      duration: true,
+    });
+    middlewares.push(logger);
+  }
+
+  const createAppStore = applyMiddleware(...middlewares)(createStore);
+  const store = createAppStore(
+    createReducer(),
+    fromJS(initialState),
+    compose(...enhancers)
+  );
+
+  // Extensions
+  store.asyncReducers = {}; // Async reducer registry
+
+  // Make reducers hot reloadable, see http://mxs.is/googmo
+  /* istanbul ignore next */
+  if (module.hot) {
+    module.hot.accept('./reducers', () => {
+      const nextRootReducer = require('./reducers').default; // eslint-disable-line global-require
+      store.replaceReducer(nextRootReducer);
+    });
+  }
+
+  return store;
+}
