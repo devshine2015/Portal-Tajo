d2f65ba31330350017caa3f6c4fb0bc992b894ab
nikita <nikita.afanasev2020@gmail.com>
2016-06-15 16:30:20 +0700

    redefine max-len eslint rule

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,7 @@
 const isDev = process.env.NODE_ENV !== 'production';
 const protocol = document.location.protocol;
+const onProduction = location.hostname === 'drvrapp.net';
 
-export const LOCAL_STORAGE_SESSION_KEY = 'ngStorage-sessionId';
-export const LOCAL_STORAGE_DATA_KEY = 'drvr_installer';
+export const LOCAL_STORAGE_SESSION_KEY = onProduction ? 'ngStorage-sessionId_tajo' : 'ngStorage-sessionId';
+export const LOCAL_STORAGE_DATA_KEY = onProduction ? 'drvr_installer_tajo' : 'drvr_installer';
 export const HOST_BASE = isDev ? `${protocol}//ddsdev.cloudapp.net:8080/engine` : '/engine';
