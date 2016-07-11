87d6ac8cc3406734d9af8b9d5646929ee590d3bd
nikita <nikita.afanasev2020@gmail.com>
2016-07-11 09:47:21 +0200

    static units list

--- src/components/UnitsList/index.js
+++ src/components/UnitsList/index.js
@@ -1,34 +1,44 @@
 import React from 'react';
 import pure from 'recompose/pure';
-import { connect } from 'react-redux';
-import ListItem from './components/ListItem';
+// import { connect } from 'react-redux';
+// import ListItem from './components/ListItem';
 import styles from './styles.css';
-import { getFleetData } from 'services/FleetModel/reducer';
+// import { getFleetData } from 'services/FleetModel/reducer';
 
 class UnitsList extends React.Component {
   render() {
-    const d = Object.values(this.props.backendData);
-
-    if (d.length !== 0) {
-      return (
+    return (
         <div className={styles.listBox}>
-          {
-            d.map(data => (
-              <div key={data.name}>{data.name}</div>
-            ))
-          }
+          <div> one </div>
+          <div> two </div>
+          <div> three </div>
         </div>
       );
-    }
-
-    return null;
   }
+  // render() {
+  //   const d = Object.values(this.props.backendData);
+  //
+  //   if (d.length !== 0) {
+  //     return (
+  //       <div className={styles.listBox}>
+  //         {
+  //           d.map(data => (
+  //             <div key={data.name}>{data.name}</div>
+  //           ))
+  //         }
+  //       </div>
+  //     );
+  //   }
+  //
+  //   return null;
+  // }
 }
 
 const PureUnitsList = pure(UnitsList);
 
-const mapState = (state) => ({
-  backendData: getFleetData(state).toJS(),
-});
+// const mapState = (state) => ({
+//   backendData: getFleetData(state).toJS(),
+// });
 
-export default connect(mapState)(PureUnitsList);
+// export default connect(mapState)(PureUnitsList);
+export default PureUnitsList;
