9358819bc0692bb904cb014aa07e3fcaa6e8f836
nikita <nikita.afanasev2020@gmail.com>
2016-08-12 18:41:42 +0700

    rename tajo -> mapViewDemo

--- webpack/webpack.prod.mapViewDemo.babel.js
+++ webpack/webpack.prod.mapViewDemo.babel.js
@@ -4,4 +4,5 @@ const path = require('path');
 module.exports = require('./webpack.prod.babel')({
   entryPoint: path.join(process.cwd(), 'src/mapViewDemo.js'),
   outputFolder: path.resolve(process.cwd(), 'mapViewDemo'),
+  projectFolder: 'mapViewDemo',
 });
