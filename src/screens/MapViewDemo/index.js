d4e035bd078051393eb5d1523e92e0bfa2be70ef
nikita <nikita.afanasev2020@gmail.com>
2016-08-18 13:14:37 +0200

    restoring GF add

--- src/screens/MapViewDemo/index.js
+++ src/screens/MapViewDemo/index.js
@@ -3,12 +3,18 @@ import pure from 'recompose/pure';
 import styles from 'screens/MapAndList/styles.css';
 import InnerPortal from 'containers/InnerPortal';
 import SplitContainer from 'containers/SplitContainer';
-// import TheMap from 'components/Map';
 import PowerListContainer from 'containers/PowerList';
-import * as ListTypes from 'containers/PowerList/types';
-import ListBox from 'containers/PowerList/components/ListBox/';
+import LocationEditPanel from 'containers/EditGFPanel';
+
+import * as listTypes from 'containers/PowerList/types';
+import * as mapEvents from 'containers/MapFleet/events';
+import * as locationEditEvents from 'containers/EditGFPanel/GFEditor/events';
 import { connect } from 'react-redux';
 import * as fromFleetReducer from 'services/FleetModel/reducer';
+
+// mode
+const MD_LIST = 'md_list';
+const MD_GF_EDIT = 'md_gfedit';
 
 const setUpHooksForMe = function (meThis) {
   return (hookId, inHook) => {
@@ -29,23 +35,54 @@ class MapAndList extends React.Component {
     super(props);
     this.hooks = {};
     this.selectedListHook = null;
+    this.state = {
+//      sidePanelMode: SMD_AREA_EDIT,
+      mode: MD_LIST,
+    };
+
+    this.hooks[mapEvents.MAP_LOCATION_ADD] = this.openGFEditor.bind(this);
+    this.hooks[locationEditEvents.GF_EDITOR_CLOSE] = this.closeGFEditor.bind(this);
+
+    this.subjGFContext = { obj: null };
+  }
+
+  openGFEditor(editFGCtx) {
+    this.subjGFContext = { obj: null, pos: editFGCtx.pos };
+    this.setState({ mode: MD_GF_EDIT });
+  }
+  closeGFEditor() {
+    this.setState({ mode: MD_LIST });
   }
 
   render() {
     return (
       <InnerPortal showPortalsList={false}>
       <div className={styles.mapAndListContainer}>
-        <PowerListContainer hooks={execHooksForMe(this)} setUpHooks={setUpHooksForMe(this)}>
-        { [{ listType: ListTypes.LIST_VEHICLES, items: this.props.vehicles },
-           { listType: ListTypes.LIST_LOCATIONS, items: this.props.locations }]
+        { this.state.mode !== MD_LIST ? null :
+          <PowerListContainer hooks={execHooksForMe(this)} setUpHooks={setUpHooksForMe(this)}>
+          { [{ listType: listTypes.LIST_VEHICLES, items: this.props.vehicles },
+             { listType: listTypes.LIST_LOCATIONS, items: this.props.locations }]
+          }
+          </PowerListContainer>
         }
-        </PowerListContainer>
-        <SplitContainer setUpHooks={setUpHooksForMe(this)} hooks={execHooksForMe(this)} />
+        { this.state.mode !== MD_GF_EDIT ? null :
+          <LocationEditPanel
+            hooks={execHooksForMe(this)}
+            setUpHooks={setUpHooksForMe(this)}
+            subjectContext={this.subjGFContext}
+          />
+        }
+        <SplitContainer
+          setUpHooks={setUpHooksForMe(this)}
+          hooks={execHooksForMe(this)}
+          gfEditMode={this.state.mode === MD_GF_EDIT}
+        />
       </div>
       </InnerPortal>
     );
   }
 }
+
 
 // const PureMapAndList = pure(MapAndList);
 // export default PureMapAndList;
