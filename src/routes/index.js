27dcedf9ab15e7340eeed76eeaaa8e6f757c1bc9
nikita <nikita.afanasev2020@gmail.com>
2016-08-22 10:45:19 +0700

    ensure merge all needed stuff from demo-map branch

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,3 +1,2 @@
 export { default as createEscapeRoutes } from './escapeRoutes';
 export { default as createPortalRoutes } from './portalRoutes';
-export { default as createPortalRoutes } from './mapViewDemoRoutes';
