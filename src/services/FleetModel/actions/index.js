703b73c683806e3c464cd3fce012c33cef37e89b
nikita <nikita.afanasev2020@gmail.com>
2016-08-22 12:00:29 +0700

    close vehicles socket on leave map screen

--- src/services/FleetModel/actions/index.js
+++ src/services/FleetModel/actions/index.js
@@ -1,3 +1,4 @@
 export * as vehiclesActions from './vehiclesActions';
 export * as gfActions from './gfActions';
 export * as commonFleetActions from './commonActions';
+export * as socketActions from './socketActions';
