c59770d8510f270ee22bfa5e40a4491226eb79f3
nikita <nikita.afanasev2020@gmail.com>
2016-09-08 22:45:18 +0800

    update gitignore for new folders structure

---
+++ src/projects/escape/index.js
@@ -0,0 +1,14 @@
+/* internal portal root */
+import configureStore from 'redux/store';
+import createReducer from 'redux/reducers';
+import { renderProject } from 'projects/baseProject';
+import createRoutes from './routes';
+
+renderProject({
+  createReducer,
+  configureStore,
+  anchor: document.getElementById('app'),
+  createRoutes,
+})
+
+
---
+++ src/projects/escape/routes.js
@@ -0,0 +1,112 @@
+import React from 'react';
+import { getHooks } from 'utils/hooks';
+import { Router, browserHistory } from 'react-router';
+import { syncHistoryWithStore } from 'react-router-redux';
+import {
+  errorHandler,
+  loadModule,
+  selectLocationState,
+} from 'utils/routerHelpers';
+
+const ROOT = '/portal/:fleet/tajo/';
+
+const MAIN_MENU = [{
+  niceName: 'dashboard',
+  path: 'dashboard',
+  order: 0,
+}, {
+  niceName: 'installer',
+  path: 'installer',
+  order: 1,
+}, {
+  niceName: 'Promo Subscribtions',
+  path: 'promos',
+  order: 2,
+}, {
+  niceName: 'Reports',
+  path: 'reports',
+  order: 3,
+}, {
+  niceName: 'Vehicles Editor',
+  path: 'vehicles',
+  order: 4,
+}, {
+  niceName: 'Users Manager',
+  path: 'users',
+  order: 5,
+}];
+
+export default function createRoutes(store) {
+  const { injectReducer } = getHooks(store);
+
+  const history = syncHistoryWithStore(browserHistory, store, {
+    selectLocationState: selectLocationState(),
+  });
+
+  const reportsRoute = require('screens/ReportsScreen/route')({
+    path: 'reports',
+    injectReducer,
+    errorHandler,
+    loadModule,
+  });
+
+  const promoRoute = require('screens/PromoTrackingScreen/route')({
+    path: 'promos',
+    injectReducer,
+    errorHandler,
+    loadModule,
+  });
+
+  const installerRoute = require('screens/InstallerScreen/route')({
+    path: 'installer',
+    injectReducer,
+    errorHandler,
+    loadModule,
+  });
+
+  const vehiclesEditorRoute = require('screens/VehiclesManagerScreen/route')({
+    path: 'vehicles',
+    injectReducer,
+    errorHandler,
+    loadModule,
+  });
+
+  // const usersManagerRoute = require('screens/Users/route')({
+  //   path: 'users',
+  // });
+
+  const loginRoute = require('screens/LoginScreen/route')({
+    path: 'login',
+  });
+
+  const dashboardRoute = require('screens/DashboardScreen/route')({
+    path: 'dashboard',
+  });
+
+  const rootRoute = require('screens/Root/route')({
+    path: ROOT,
+    dispatch: store.dispatch,
+    mainMenu: MAIN_MENU,
+  });
+
+  rootRoute.indexRoute = {
+    component: require('screens/DashboardScreen').default,
+  };
+
+  rootRoute.childRoutes.push(
+    loginRoute,
+    dashboardRoute,
+    installerRoute,
+    promoRoute,
+    reportsRoute,
+    vehiclesEditorRoute,
+    // usersManagerRoute,
+  );
+
+  return (
+    <Router
+      history={history}
+      // routes={rootRoute}
+    />
+  );
+}
