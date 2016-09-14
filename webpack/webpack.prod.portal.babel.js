1337405e7eecc669123f15f662c4f4f9c763ec1f
nikita <nikita.afanasev2020@gmail.com>
2016-09-14 14:39:52 +0700

    temporary solution for deploying customer portal 3

--- webpack/webpack.prod.portal.babel.js
+++ webpack/webpack.prod.portal.babel.js
@@ -3,6 +3,6 @@ const path = require('path');
 
 module.exports = require('./webpack.prod.babel')({
   entryPoint: path.join(process.cwd(), 'src/projects/portal'),
-  outputFolder: path.resolve(process.cwd(), 'customer'),
-  projectFolder: 'tajo',
+  outputFolder: path.resolve(process.cwd(), 'portal'),
+  projectFolder: 'portal',
 });
