ba5e66a5c80d94bdb502de37b2970a2ca592194d
nikita <nikita.afanasev2020@gmail.com>
2016-09-15 12:35:56 +0700

    tuning deployment scripts 7

--- ci/deploy-prod.sh
+++ ci/deploy-prod.sh
@@ -12,10 +12,7 @@ npm run build:$PROJECT1
 npm run build:$PROJECT2
 
 echo "erase projects on server"
-ssh $1 "cd $TARGET_FOLDER && rm -r $PROJECT1 $PROJECT2 && mkdir $PROJECT1 $PROJECT2"
+ssh $1 "cd $TARGET_FOLDER && rm -r $PROJECT1 $PROJECT2"
 
-echo "copy files of $PROJECT1 to $TARGET_FOLDER/$PROJECT1"
-scp "$FOLDER_NAME1/* $1:$TARGET_FOLDER/$PROJECT1"
-
-echo "copy files of $PROJECT2 to $TARGET_FOLDER/$PROJECT2"
-scp "$FOLDER_NAME2/* $1:$TARGET_FOLDER/$PROJECT2"
+echo "copy files of $PROJECT1 and $PROJECT2 to $TARGET_FOLDER"
+scp -r $FOLDER_NAME1 $FOLDER_NAME2 $1:$TARGET_FOLDER
