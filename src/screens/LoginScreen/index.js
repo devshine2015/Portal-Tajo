096c548185133442c0ba1130cdc3fc73babcce22
nikita <nikita.afanasev2020@gmail.com>
2016-09-19 18:13:48 +0700

    hide links to other portals on production

--- src/screens/LoginScreen/index.js
+++ src/screens/LoginScreen/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import pure from 'recompose/pure';
 import LoginForm from 'containers/LoginForm';
 import PortalsLinks from 'components/PortalsLinks';
+import { onProduction } from 'configs';
 
 const LoginScreenDemo = () => (
   <div>
     <LoginForm />
-    <PortalsLinks />
+    { !onProduction && <PortalsLinks /> }
   </div>
 );
 
