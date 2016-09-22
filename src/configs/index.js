05202832ecbb18106dd1fdb23c501fa0bd792ca8
nikita <nikita.afanasev2020@gmail.com>
2016-09-22 10:39:22 +0700

    shorthands for servefEnvironments; reset local storage sessionId key notation

--- src/configs/index.js
+++ src/configs/index.js
@@ -1,7 +1,7 @@
 const DEV_ENGINE_BASE = 'ddsdev.cloudapp.net:8080/engine';
 const PROD_ENGINE_BASE = `${window.location.host}/engine`;
-const chooseServerEnv = onProduction => {
-  if (onProduction) {
+const chooseServerEnv = () => {
+  if (window.location.hostname.search('drvrapp.net') !== -1) {
     return 'production';
   } else if (window.location.hostname.search('drvrstage') !== -1) {
     return 'stage';
@@ -16,15 +16,12 @@ export const isDev = process.env.NODE_ENV !== 'production';
 export const protocol = document.location.protocol;
 export const isSecure = protocol.search('https') !== -1;
 export const socketProtocol = isSecure ? 'wss' : 'ws';
-export const onProduction = location.hostname === 'drvrapp.net';
-export const serverEnv = chooseServerEnv(onProduction);
-export const LOCAL_STORAGE_SESSION_KEY = onProduction && portal !== 'ssreports' ?
-  'ngStorage-sessionId_tajo' : 'ngStorage-sessionId';
-export const FIREBASE_CONFIG = {
-  apiKey: 'AIzaSyBp0Tvsnkrnv9mpFK6Zo0U9VdHZqH2hKjA',
-  authDomain: 'drvr.firebaseapp.com',
-  databaseURL: 'https://drvr.firebaseio.com',
-  storageBucket: 'project-7060603335742899024.appspot.com',
-};
+export const serverEnv = chooseServerEnv();
+export const onProduction = serverEnv === 'production';
+export const onStage = serverEnv === 'stage';
+export const onDev = serverEnv === 'dev';
+// use old local storage key notation for ssreports
+export const LOCAL_STORAGE_SESSION_KEY = portal !== 'ssreports' ?
+  'drvr_tajo-sessionId' : 'ngStorage-sessionId';
 
 export const ENGINE_BASE = onProduction ? PROD_ENGINE_BASE : DEV_ENGINE_BASE;
