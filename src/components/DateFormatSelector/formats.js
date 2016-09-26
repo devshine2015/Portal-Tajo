38926c6148808e53692bf83fde04e917c1acea9e
nikita <nikita.afanasev2020@gmail.com>
2016-09-26 13:06:35 +0700

    create date format selector

---
+++ src/components/DateFormatSelector/formats.js
@@ -0,0 +1,7 @@
+export default [{
+  text: 'yyyy-mm-dd (Burmese)',
+  value: 'yyyy-mm-dd',
+}, {
+  text: 'dd-mm-yyyy (Gregorian)',
+  value: 'dd-mm-yyyy',
+}];
---
+++ src/components/DateFormatSelector/index.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import pure from 'recompose/pure';
+import {
+  SelectField,
+  MenuItem,
+} from 'material-ui';
+import formats from './formats';
+
+import styles from './styles.css';
+
+function renderItems() {
+  return formats.map(format => (
+    <MenuItem
+      key={format.value}
+      value={format.value}
+      primaryText={format.text}
+    />
+  ));
+}
+
+class DateFormatSelector extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: props.defaultValue || formats[0].value,
+    };
+  }
+
+  onChange = (e, key, value) => {
+    this.setState({
+      value,
+    }, () => {
+      this.props.onChange(e, key, value);
+    });
+  }
+  render() {
+    return (
+      <div className={styles.selector}>
+        <SelectField
+          floatingLabelFixed
+          floatingLabelText="Choose date format for the report"
+          onChange={this.onChange}
+          value={this.state.value}
+        >
+          { renderItems() }
+        </SelectField>
+      </div>
+    );
+  }
+}
+
+DateFormatSelector.propTypes = {
+  defaultValue: React.PropTypes.oneOf([
+    'yyyy-mm-dd', 'dd-mm-yyyy',
+  ]),
+  onChange: React.PropTypes.func.isRequired,
+};
+
+export default pure(DateFormatSelector);
