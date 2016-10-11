57111a23caa08d5dbbef9752bcb6ede0025f3249
nikita <nikita.afanasev2020@gmail.com>
2016-10-11 12:44:24 +0700

    add /tajo to links in internal portal

--- src/utils/createBaseUrl.js
+++ src/utils/createBaseUrl.js
@@ -1,5 +1,8 @@
-import { portal } from 'configs';
+import { portal, onDev } from 'configs';
 
 export default function createBaseUrl(fleet = undefined) {
+  // on dev environments we don't have fleet in url
+  if (onDev && portal === 'tajo') return '/tajo';
+
   return !fleet ? '' : `/portal/${fleet}/${portal}`;
 }
