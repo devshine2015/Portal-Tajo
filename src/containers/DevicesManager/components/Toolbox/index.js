e3e8fcf4a9e3b2a9e26fb372f4f6d798b9ed550a
nikita <nikita.afanasev2020@gmail.com>
2016-10-14 23:59:44 +0700

    display filter toolbox total

--- src/containers/DevicesManager/components/Toolbox/index.js
+++ src/containers/DevicesManager/components/Toolbox/index.js
@@ -9,15 +9,37 @@ import {
 
 import styles from './styles.css';
 
+const TotalLink = ({ onClick, text }) => {
+  const isZero = text === 0 || text === '0';
+
+  if (isZero) {
+    return <span>{text}</span>;
+  }
+
+  return (
+    <span
+      className={styles.total__link}
+      onClick={onClick}
+    >
+      {text}
+    </span>
+  );
+};
+
+TotalLink.propTypes = {
+  onClick: React.PropTypes.func.isRequired,
+  text: React.PropTypes.number.isRequired,
+};
+
 const Total = ({
   totalDevices,
   totalFaultVehicles,
   totalNotAttached,
 }) => (
   <div className={styles.total}>
-    Total devices: {totalDevices},
-    Not attached: {totalNotAttached},
-    Attached to wrong vehicle: {totalFaultVehicles}.
+    Total devices: <TotalLink onClick={() => {}} text={totalDevices} />,
+    Not attached: <TotalLink onClick={() => {}} text={totalNotAttached} />,
+    Attached to wrong vehicle: <TotalLink onClick={() => {}} text={totalFaultVehicles} />.
   </div>
 );
 
