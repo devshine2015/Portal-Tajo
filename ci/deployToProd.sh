c464ca44332d9d096ee32eaac84b46e5da786687
nikita <nikita.afanasev2020@gmail.com>
2016-10-19 19:29:38 +0700

    deploy to production script

---
+++ ci/deployToProd.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+TAJO=$2
+PORTAL=$3
+TARGET_FOLDER=$4
+TAJO_FOLDER="builds/prod/$TAJO"
+PORTAL_FOLDER="builds/prod/$PORTAL"
+
+echo "clean and build sources"
+npm run clean
+npm run build:$TAJO
+npm run build:$PORTAL
+
+echo "erase projects on server"
+ssh $1 "cd $TARGET_FOLDER && rm -r $TAJO $PORTAL"
+
+echo "copy files of $TAJO and $PORTAL to $TARGET_FOLDER"
+scp -r $PORTAL_FOLDER $TAJO_FOLDER $1:$TARGET_FOLDER
