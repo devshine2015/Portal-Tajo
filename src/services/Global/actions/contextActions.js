34a36e10ea97481ad6cb999cffc5ac45abc5ebf8
nikita <nikita.afanasev2020@gmail.com>
2016-11-10 06:08:56 +0100

    context actions/reducer

--- src/services/Global/actions/contextActions.js
+++ src/services/Global/actions/contextActions.js
@@ -1,8 +1,22 @@
-export const MAP_STOREVIEW = 'map/storeView';
+export const CTX_MAP_STOREVIEW = 'ctx/map/storeView';
+export const CTX_HIDE_GF = 'ctx/hideGf';
+export const CTX_HIDE_VEH = 'ctx/hideVeh';
 
 export const ctxStoreMap = (center, zoom) => (dispatch) =>
   dispatch({
-    type: MAP_STOREVIEW,
+    type: CTX_MAP_STOREVIEW,
     center,
     zoom,
+  });
+
+export const ctxHideGF = (doHide) => (dispatch) =>
+  dispatch({
+    type: CTX_HIDE_GF,
+    doHide,
+  });
+
+export const ctxHideVehicles = (doHide) => (dispatch) =>
+  dispatch({
+    type: CTX_HIDE_VEH,
+    doHide,
   });
--- src/services/Global/reducers/contextReducer.js
+++ src/services/Global/reducers/contextReducer.js
@@ -1,42 +1,39 @@
-// // all the params related to user selections - store it to
-// // make sure UX is persistent
-//
-// import { ZERO_LOCATION, ZERO_ZOOM } from 'utils/constants';
-//
-// import { fromJS } from 'immutable';
-// import { contextActions } from '../actions';
-//
-// const initialState = fromJS({
-//   map: {
-//     center: ZERO_LOCATION,
-//     zoom: ZERO_ZOOM,
-//   },
-//   filter: {
-//     string: '',
-//     presets: null,
-//   },
-//   selectedVehicleId: '',
-//   selectedGFId: '',
-//   hideGF: false,
-//   hideVehicles: false,
-// });
-//
-// function contextReducer(state = initialState, action) {
-//   switch (action.type) {
-//     case contextActions.MAP_STOREVIEW:
-//       return state{
-//         center: action.center,
-//         zoom: action.zoom,
-//       };
-//     default:
-//       return state;
-//   }
-// }
-// export const ctxMapView = (state) =>
-//   state.getIn(['mapView']);
-//
-// export default contextReducer;
-//
+// all the params related to user selections - store it to
+// make sure UX is persistent
+
+import { ZERO_LOCATION, ZERO_ZOOM } from 'utils/constants';
+
+import { fromJS } from 'immutable';
+import { contextActions } from '../actions';
+
+const initialState = fromJS({
+  map: {
+    center: ZERO_LOCATION,
+    zoom: ZERO_ZOOM,
+  },
+  selectedVehicleId: '',
+  selectedGFId: '',
+  hideGF: false,
+  hideVehicles: false,
+});
+
+function contextReducer(state = initialState, action) {
+  switch (action.type) {
+    case contextActions.CTX_MAP_STOREVIEW:
+    // TODO: implement!
+      return state;
+    case contextActions.CTX_HIDE_GF:
+      return state.set('hideGF', action.doHide);
+    default:
+      return state;
+  }
+}
+
+export const ctxMapView = (state) =>
+  state.getIn(['mapView']);
+
+export default contextReducer;
+
 export const ctxGetMap = state =>
   state.getIn(['global', 'context']).map;
 export const ctxGetSelectedVehicleId = state =>
