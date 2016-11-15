2c9d00f56146d2a289658a9b7698e41001fe24d0
nikita <nikita.afanasev2020@gmail.com>
2016-11-15 08:07:14 +0100

    map marker toggle btn

--- src/components/MapMarkerToggle/index.js
+++ src/components/MapMarkerToggle/index.js
@@ -2,6 +2,14 @@ import React from 'react';
 import pure from 'recompose/pure';
 import { connect } from 'react-redux';
 
+import IconButton from 'material-ui/IconButton';
+import DeletIcon from 'material-ui/svg-icons/action/delete-forever';
+// import CarIcon from 'material-ui/svg-icons/maps/directions-car';
+import CarIcon from 'material-ui/svg-icons/maps/local-shipping';
+// import LocationIcon from 'material-ui/svg-icons/social/location-city';
+import LocationIcon from 'material-ui/svg-icons/social/location-city';
+import { red900, red500, teal100, teal200, yellow700, yellow500 } from 'material-ui/styles/colors';
+
 import { ctxGetHideGF, ctxGetHideVehicles, ctxGetPowListTabType } from 'services/Global/reducers/contextReducer';
 import { contextActions } from 'services/Global/actions';
 
@@ -13,13 +21,14 @@ class MapMarkerToggle extends React.Component {
 
   onClick = () => {
     switch (this.props.listType) {
-      case listTypes.withVehicleDetails:
-        this.props.doHideGF(!this.props.isHideGF);
-        break;
       case listTypes.withGFDetails:
         this.props.doHideVehicles(!this.props.isHideVehicles);
         break;
+      // TODO: for now, hide GFs if unknown list type (for chronicle view)
+      case listTypes.withVehicleDetails:
       default:
+        this.props.doHideGF(!this.props.isHideGF);
+        break;
     }
   }
 
@@ -28,25 +37,45 @@ class MapMarkerToggle extends React.Component {
 
   contentText = () => {
     switch (this.props.listType) {
-      case listTypes.withVehicleDetails:
-        return this.hidePrefix(this.props.isHideGF) + 'Locations';
       case listTypes.withGFDetails:
         return this.hidePrefix(this.props.isHideVehicles) + 'Vehicles';
+      case listTypes.withVehicleDetails:
       default:
+        return this.hidePrefix(this.props.isHideGF) + 'Locations';
     }
-    return '';
+  }
+  contentIcon = () => {
+    switch (this.props.listType) {
+      case listTypes.withGFDetails:
+        return (<CarIcon color={yellow700} hoverColor={yellow500} />);
+      case listTypes.withVehicleDetails:
+      default:
+        return (<LocationIcon color={yellow700} hoverColor={yellow500} />);
+    }
   }
 
   render() {
     return (
-      <button
-        className={styles.toggle}
+      <IconButton
+        tooltip="Toggle"
         onClick={this.onClick}
+        className={styles.iconBtn}
+        key="delBtn"
       >
-      {this.contentText()}
-      </button>
+        {this.contentIcon()}
+      </IconButton>
     );
   }
+  // render() {
+  //   return (
+  //     <button
+  //       className={styles.toggle}
+  //       onClick={this.onClick}
+  //     >
+  //     {this.contentText()}
+  //     </button>
+  //   );
+  // }
 }
 
 MapMarkerToggle.propTypes = {
