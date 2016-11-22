ec047e0b0f0d34236258e25b589791ca2d572d99
nikita <nikita.afanasev2020@gmail.com>
2016-11-22 11:37:04 +0700

    dev deployment

--- ci/deployToDev.sh
+++ ci/deployToDev.sh
@@ -1,26 +1,9 @@
 #!/usr/bin/env bash
+
 HOST='ddsdev@ddsdev.cloudapp.net'
-TAJO=$1
-PORTAL=$2
-EXTRA_FOLDER=$3
+PUBLIC='~/driver-server/static-root/public'
+EXTRA_FOLDER=$1
 
-if [ -z $EXTRA_FOLDER ] ; then
-  TARGET_FOLDER="~/driver-server/static-root/public/$EXTRA_FOLDER"
-else
-  TARGET_FOLDER="~/driver-server/static-root/public"
-fi
+parent_path=$( cd "$(dirname "${BASH_SOURCE}")" ; pwd -P )
 
-TAJO_FOLDER="builds/prod/$TAJO"
-PORTAL_FOLDER="builds/prod/$PORTAL"
-
-echo "clean and build sources"
-npm run clean
-npm run build:$TAJO
-npm run build:$PORTAL
-
-echo "erase projects on server"
-ssh $HOST "cd $TARGET_FOLDER && rm -r $TAJO $PORTAL"
-
-echo "copy files of customer portal to root\ntajo to $TARGET_FOLDER"
-scp -r $PORTAL_FOLDER/* $HOST:$TARGET_FOLDER
-scp -r $TAJO_FOLDER $HOST:$TARGET_FOLDER
+sh $parent_path/commonDeploy.sh $HOST $PUBLIC $EXTRA_FOLDER
