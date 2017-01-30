f574f1fc6d8233395c65fd386a158c62c29b1706
nikita <nikita.afanasev2020@gmail.com>
2017-01-30 10:50:54 +0700

    Change git commands order for versioning

--- ci/versions.sh
+++ ci/versions.sh
@@ -2,9 +2,9 @@
 
 V=$1
 
-npm version $V
-
-git config --global user.email "orelmax@gmail.com"
+git config --global user.email "circle@circle.drvr.co"
 git config --global user.name "$CIRCLE_USERNAME"
+
+npm version $V
 
 git push origin $CIRCLE_BRANCH
