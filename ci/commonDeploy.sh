c76d6a147233833839621e8934ba63b2cb4dcca2
nikita <nikita.afanasev2020@gmail.com>
2017-02-01 17:26:31 +0700

    Next try on production deployment fixing

--- ci/commonDeploy.sh
+++ ci/commonDeploy.sh
@@ -4,7 +4,7 @@ TAJO='tajo'
 PORTAL='portal'
 HOST=$1
 PUBLIC=$2
-ENV=$3
+SERVER_ENV=$3
 EXTRA_FOLDER=$4
 
 if [ ! -z $EXTRA_FOLDER ] ; then
@@ -13,15 +13,15 @@ else
   TARGET_FOLDER=$PUBLIC
 fi
 
-TAJO_FOLDER="builds/$ENV/$TAJO"
-PORTAL_FOLDER="builds/$ENV/$PORTAL"
+TAJO_FOLDER="builds/$SERVER_ENV/$TAJO"
+PORTAL_FOLDER="builds/$SERVER_ENV/$PORTAL"
 
 echo "rebuild static sources..."
 npm run clean
 
-echo $ENV
+echo $SERVER_ENV
 
-if [ "$ENV" == "prod" ] ; then
+if [ $SERVER_ENV = "prod" ] ; then
   npm run build:escape
   npm run build:sunshine
 else
@@ -30,23 +30,23 @@ else
 fi
 
 
-# if [ ! -z $EXTRA_FOLDER ] ; then
-#   echo "Extra folder $EXTRA_FOLDER detected"
+if [ ! -z $EXTRA_FOLDER ] ; then
+  echo "Extra folder $EXTRA_FOLDER detected"
 
-#   if (ssh $HOST "[ -d $PUBLIC/$EXTRA_FOLDER ]"); then
-#     echo "$EXTRA_FOLDER already exists - remove and create again..."
-#     ssh $HOST "cd $PUBLIC && rm -r $EXTRA_FOLDER && mkdir $EXTRA_FOLDER"
-#   else
-#     echo "$EXTRA_FOLDER not exist - creating..."
-#     ssh $HOST "cd $PUBLIC && mkdir $EXTRA_FOLDER"
-#   fi
+  if (ssh $HOST "[ -d $PUBLIC/$EXTRA_FOLDER ]"); then
+    echo "$EXTRA_FOLDER already exists - remove and create again..."
+    ssh $HOST "cd $PUBLIC && rm -r $EXTRA_FOLDER && mkdir $EXTRA_FOLDER"
+  else
+    echo "$EXTRA_FOLDER not exist - creating..."
+    ssh $HOST "cd $PUBLIC && mkdir $EXTRA_FOLDER"
+  fi
 
-# else
-#   echo "erase root tajo and portal files..."
-#   ssh $HOST "cd $PUBLIC && rm -r $TAJO js css fonts .htaccess index.html manifest.json favicon.ico"
-# fi
+else
+  echo "erase root tajo and portal files..."
+  ssh $HOST "cd $PUBLIC && rm -r $TAJO js css fonts .htaccess index.html manifest.json favicon.ico"
+fi
 
 
-# echo "copy static files to server..."
-# scp -r $PORTAL_FOLDER/* $HOST:$TARGET_FOLDER
-# scp -r $TAJO_FOLDER $HOST:$TARGET_FOLDER
+echo "copy static files to server..."
+scp -r $PORTAL_FOLDER/* $HOST:$TARGET_FOLDER
+scp -r $TAJO_FOLDER $HOST:$TARGET_FOLDER
