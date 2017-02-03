f0ad66118cda9d7dddc5dd93407537424495de92
nikita <nikita.afanasev2020@gmail.com>
2017-02-03 14:30:56 +0700

    Config eslint for mocha tests

--- src/configs/specs/index.spec.js
+++ src/configs/specs/index.spec.js
@@ -1,11 +1,10 @@
-// const assert = require('assert');
+/* eslint-disable prefer-arrow-callback, func-names */
 import assert from 'assert';
 
-
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1,2,3].indexOf(4));
+describe('Array', function () {
+  describe('#indexOf()', function () {
+    it('should return -1 when the value is not present', function () {
+      assert.equal(-1, [1, 2, 3].indexOf(4));
     });
   });
 });
