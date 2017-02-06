1fe203cad47c4228cdfb0c966c6cbe730cfe3117
nikita <nikita.afanasev2020@gmail.com>
2017-02-06 15:58:36 +0700

    test(vehicleHelpers): Tests for ignition status checking

--- src/services/FleetModel/utils/specs/vehicleHelpers.spec.js
+++ src/services/FleetModel/utils/specs/vehicleHelpers.spec.js
@@ -2,7 +2,7 @@
 import chai from 'chai';
 import { _private as helpers } from '../vehicleHelpers';
 
-const should = chai.should();
+const should = chai.should(); // eslint-disable-line no-unused-vars
 
 describe('Fleet model vehicle helpers', function() {
   describe('checkIgnition', function() {
@@ -13,6 +13,24 @@ describe('Fleet model vehicle helpers', function() {
       const result = _checkIgnition(status);
 
       result.should.be.equal(2);
+    });
+
+    it('should return 1 if `ignOn` is true', function() {
+      const status = {
+        ignOn: true,
+      };
+      const result = _checkIgnition(status);
+
+      result.should.be.equal(1);
+    });
+
+    it('should return 0 if `ignOn` is false', function() {
+      const status = {
+        ignOn: false,
+      };
+      const result = _checkIgnition(status);
+
+      result.should.be.equal(0);
     });
   });
 });
