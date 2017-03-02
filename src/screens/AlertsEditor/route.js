04e3dd6f02a5db88c65bd6617937e26c86f35197
nikita <nikita.afanasev2020@gmail.com>
2017-03-02 11:53:15 +0100

    feat(empty alerts editor):

--- src/screens/AlertsEditor/route.js
+++ src/screens/AlertsEditor/route.js
@@ -1,18 +1,14 @@
 const createRoute = ({
   path,
   name,
-  niceName,
 }) => ({
   path,
   name,
-  niceName,
   getComponent: (location, cb) => {
     require.ensure([], require => {
       cb(null, require('./index').default);
     }, 'alerts');
   },
-  childRoutes: [],
-  protected: true,
 });
 
-module.exports = createRoute;
+export default createRoute;
