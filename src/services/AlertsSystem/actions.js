ab0c5a82cf8fb3649dfaf3df5df7e41c57e173d6
nikita <nikita.afanasev2020@gmail.com>
2017-03-05 10:49:37 +0100

    alertsEditor - new alert

--- src/services/AlertsSystem/actions.js
+++ src/services/AlertsSystem/actions.js
@@ -1,16 +1,16 @@
-import { makeLocalAlertCondition } from './alertConditionHelper';
+import { _dev_makeLocalAlertCondition } from './alertConditionHelper';
 
 export const ALRT_TYPES_SET = 'alrt/typesSet';
 export const ALRT_CONDITON_ADD = 'alrt/conditionAdd';
 export const ALRT_EVEENTS_ADD = 'alrt/eventsAdd';
 
 export const fetchAlertConditions = () => (dispatch) => {
-  dispatch(_conditionAdd(makeLocalAlertCondition('0001', 'Temperature MedRange')));
-  dispatch(_conditionAdd(makeLocalAlertCondition('0002', 'Temperature SubZero')));
-  dispatch(_conditionAdd(makeLocalAlertCondition('0003', 'Speed Low')));
-  dispatch(_conditionAdd(makeLocalAlertCondition('0004', 'Speed High')));
-  dispatch(_conditionAdd(makeLocalAlertCondition('0005', 'Left Warehause')));
-  dispatch(_conditionAdd(makeLocalAlertCondition('0006', 'Enter PortArea')));
+  dispatch(_conditionAdd(_dev_makeLocalAlertCondition('0001', 'Temperature MedRange', 'TEMPERATURE')));
+  dispatch(_conditionAdd(_dev_makeLocalAlertCondition('0002', 'Temperature SubZero', 'TEMPERATURE')));
+  dispatch(_conditionAdd(_dev_makeLocalAlertCondition('0003', 'Speed Low', 'SPEED')));
+  dispatch(_conditionAdd(_dev_makeLocalAlertCondition('0004', 'Speed High', 'SPEED')));
+  dispatch(_conditionAdd(_dev_makeLocalAlertCondition('0005', 'Left Warehause', 'EXIT_GF')));
+  dispatch(_conditionAdd(_dev_makeLocalAlertCondition('0006', 'Enter PortArea', 'ENTER_GF')));
 };
 
 // export const addAlertCondition = (alertObj) => dispatch => {
