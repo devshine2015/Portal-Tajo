6a5e073955270c2c81f7a7f79d8612b627c57202
nikita <nikita.afanasev2020@gmail.com>
2017-03-09 18:36:09 +0100

    implementing alertsAPI

--- src/services/AlertsSystem/actions.js
+++ src/services/AlertsSystem/actions.js
@@ -1,21 +1,77 @@
-import { _dev_makeLocalAlertCondition } from './alertConditionHelper';
+  // {
+  //   "aboveTemp": -1,
+  //   "created": "2017-03-06T18:29:50.950+1100",
+  //   "status": "",
+  //   "meta": {
+  //     "key1": "val1",
+  //     "key2": "val2"
+  //   },
+  //   "kind": "temperature-alert"
+  // },
+  // {
+  //   "gfId": "123456",
+  //   "created": "2017-03-06T18:29:50.952+1100",
+  //   "status": "",
+  //   "meta": {
+  //     "key1": "val1",
+  //     "key2": "val2"
+  //   },
+  //   "kind": "geofence-alert"
+  // }
+
+import endpoints from 'configs/endpoints';
+import api from 'utils/api';
+import { makeLocalAlertCondition } from './alertConditionHelper';
 
 export const ALRT_TYPES_SET = 'alrt/typesSet';
 export const ALRT_CONDITON_ADD = 'alrt/conditionAdd';
 export const ALRT_EVEENTS_ADD = 'alrt/eventsAdd';
 
-export const fetchAlertConditions = () => (dispatch) => {
-  dispatch(_conditionAdd(_dev_makeLocalAlertCondition('0001', 'Temperature MedRange', 'TEMPERATURE')));
-  dispatch(_conditionAdd(_dev_makeLocalAlertCondition('0002', 'Temperature SubZero', 'TEMPERATURE')));
-  dispatch(_conditionAdd(_dev_makeLocalAlertCondition('0003', 'Speed Low', 'SPEED')));
-  dispatch(_conditionAdd(_dev_makeLocalAlertCondition('0004', 'Speed High', 'SPEED')));
-  dispatch(_conditionAdd(_dev_makeLocalAlertCondition('0005', 'Left Warehause', 'EXIT_GF')));
-  dispatch(_conditionAdd(_dev_makeLocalAlertCondition('0006', 'Enter PortArea', 'ENTER_GF')));
-};
+export const createAlertConditions = (newAlerts) => (dispatch) =>
+  _createAlertRequest(newAlerts, dispatch);
+
+export const fetchAlertConditions = () => _fetchAlerts;
+
+function _fetchAlerts(dispatch) {
+  const { url, method } = endpoints.getAlertConditions;
+
+  return api[method](url)
+    .then(toJson)
+    .then(alerts => {
+      _addAlerts(dispatch, alerts);
+    })
+    .catch(e => {
+      console.error(e);
+    });
+}
+
+function _addAlerts(dispatch, backEndAlerts) {
+  backEndAlerts.forEach((aElement) => {
+    dispatch(_conditionAdd(makeLocalAlertCondition(aElement)));
+  }, this);
+}
+
+function toJson(response) {
+  return response.json();
+}
 
 // export const addAlertCondition = (alertObj) => dispatch => {
 //   dispatch(_conditionAdd(alertObj));
 // };
+
+/**
+ * POST - new Alerts details to the server
+ **/
+function _createAlertRequest(alertObject, dispatch) {
+  const { url, method } = endpoints.createAlertConditions;
+
+  return api[method](url, {
+    payload: alertObject,
+  }).then(() => {
+    _fetchAlerts(dispatch);
+    return Promise.resolve();
+  }, error => Promise.reject(error));
+}
 
 const _conditionAdd = (alertObj) => ({
   type: ALRT_CONDITON_ADD,
--- src/services/AlertsSystem/alertKinds.js
+++ src/services/AlertsSystem/alertKinds.js
@@ -1,41 +1,45 @@
 import React from 'react';
 
-import Sgv from 'components/Icons/sgv.svg';
-import SgvChilled from 'components/Icons/sgv_chilled.svg';
-import SgvFrozen from 'components/Icons/sgv_frozen.svg';
+// import Sgv from 'components/Icons/sgv.svg';
+// import SgvFrozen from 'components/Icons/sgv_frozen.svg';
 import Undefined from 'components/Icons/undefined.svg';
+import IconSnow from 'material-ui/svg-icons/places/ac-unit';
+import IconLocation from 'material-ui/svg-icons/maps/pin-drop';
 
-import iSgv from 'assets/images/v_icons_combi/sgv@3x.png';
-import iSgvChilled from 'assets/images/v_icons_combi/sgv_chilled@3x.png';
-import iSgvFrozen from 'assets/images/v_icons_combi/sgv_frozen@3x.png';
-import iUndefined from 'assets/images/v_icons_combi/uncknown@3x.png';
+export const _ALERT_KIND_UNDEFINED = 'undefined-alert';
+export const _ALERT_KIND_TEMPERATURE = 'temperature-alert';
+export const _ALERT_KIND_GF_ENTER = 'geofence-alert';
+
 
 const undefinedType = {
-  value: 'UNDEFINED',
+  value: _ALERT_KIND_UNDEFINED,
   icon: <Undefined />,
-  pic: iUndefined,
 };
 
 export const ALERT_KINDS = [
   {
-    value: 'TEMPERATURE',
-    icon: <SgvFrozen />,
-    pic: iSgvFrozen,
+    value: _ALERT_KIND_TEMPERATURE,
+    niceName: 'Temperature',
+    icon: <IconSnow />,
+    // making back-end object for particular kind
+    makeBEObject: (inState) => (
+      {
+        aboveTemp: Math.round(inState.maxTemp),
+      }
+    ),
   }, {
-    value: 'SPEED',
-    icon: <Sgv />,
-    pic: iSgv,
-  }, {
-    value: 'ENTER_GF',
-    icon: <SgvChilled />,
-    pic: iSgvChilled,
-  }, {
-    value: 'EXIT_GF',
-    icon: <SgvChilled />,
-    pic: iSgvChilled,
+    value: _ALERT_KIND_GF_ENTER,  // enterGF
+    niceName: 'Enter GF',
+    icon: <IconLocation />,
+    makeBEObject: (inState) => (
+      {
+        gfId: inState.gfId,
+      }
+    ),
+
   },
 ];
 
-export function getAlertByValue(value) {
+export function getAlertByKind(value) {
   return ALERT_KINDS.filter(kind => kind.value === value)[0] || undefinedType;
 }
