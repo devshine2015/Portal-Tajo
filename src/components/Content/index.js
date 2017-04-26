5f7f7b8899ce5098ab11d824d0ef53e12c8357ef
nikita <nikita.afanasev2020@gmail.com>
2017-04-26 10:38:47 +0200

    maxWidth for content component

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -6,13 +6,14 @@ import classes from './classes';
 const Content = ({
   children,
   center,
+  maxWidth,
 }) => {
   const className = cs(css(classes.content), {
     [css(classes.content_center)]: center,
   });
 
   return (
-    <div className={className}>
+    <div className={className} style={maxWidth !== undefined ? { maxWidth } : {}}>
       { children }
     </div>
   );
@@ -25,6 +26,7 @@ Content.defaultProps = {
 Content.propTypes = {
   children: React.PropTypes.any.isRequired,
   center: React.PropTypes.bool,
+  maxWidth: React.PropTypes.number,
 };
 
 export default Content;
