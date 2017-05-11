77cd757642452fe59bbaea7bc53825afc9f8e504
nikita <nikita.afanasev2020@gmail.com>
2017-05-11 10:10:48 +0200

    remove FetchAlertConditions from FetchFleet

--- src/services/FleetModel/actions/commonActions.js
+++ src/services/FleetModel/actions/commonActions.js
@@ -1,9 +1,8 @@
-import { conditionsActions } from 'services/AlertsSystem/actions';
 import { fetchVehicles } from './vehiclesActions';
 import { fetchGFs } from './gfActions';
 
 export const fetchFleet = () => (dispatch) => {
-  dispatch(conditionsActions.fetchAlertConditions());
   dispatch(fetchGFs());
   return dispatch(fetchVehicles());
 };
+
