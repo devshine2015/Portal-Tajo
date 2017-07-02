5f55658586ffb06eb6534d984d6aee0d80cf44e1
nikita <nikita.afanasev2020@gmail.com>
2017-07-02 12:31:05 +0200

    report table with idles; PDF print btn

--- src/screens/ExecReports/components/UglyTable/UglyTable.js
+++ src/screens/ExecReports/components/UglyTable/UglyTable.js
@@ -8,6 +8,8 @@ import { metersToKmString, speedToString, msToTimeIntervalString,
   dateToHHMM, temperatureToString } from 'utils/convertors';
 
 import Layout from 'components/Layout';
+import MainActionButton from 'components/Controls/MainActionButton';
+import generatReportPDF from './UglyTablePDF';
 
 import {
   Table,
@@ -22,6 +24,7 @@ import { getInstanceExecReportFrameById } from './../../services/reducer';
 import styles from './styles.css';
 
 const btmColorStyle = { borderBottomColor: 'gray' };
+const totalRowStyle = { WebkitPrintColorAdjust: 'exact', backgroundColor: 'beige', fontWeight: 'bold' };
 // import classes from './classes';
 
 const cellStyle = {
@@ -40,18 +43,24 @@ const nameCellStyle = {
   fontSize: '11px',
 };
 
+const boldStyle = {
+  fontWeight: 'bold',
+};
+
 const TripRow = ({
   aTrip,
 }) => (
   <TableRow className={styles.row} style={btmColorStyle}>
+    <TableRowColumn style={cellStyle}>{msToTimeIntervalString(aTrip.fromStopOwer.durationMs)}</TableRowColumn>
     <TableRowColumn style={cellStyle}>{dateToHHMM(aTrip.startDate)}</TableRowColumn>
     <TableRowColumn style={nameCellStyle}>{aTrip.fromStopOwer.address}</TableRowColumn>
     <TableRowColumn style={cellStyle}>{dateToHHMM(aTrip.endDate)}</TableRowColumn>
     <TableRowColumn style={nameCellStyle}>{aTrip.toStopOver.address}</TableRowColumn>
-    <TableRowColumn style={cellStyle}>{msToTimeIntervalString(aTrip.durationMs)}</TableRowColumn>
+    <TableRowColumn style={cellStyle}>{msToTimeIntervalString(aTrip.calculatedOperationalDurationMs)}</TableRowColumn>
+    <TableRowColumn style={cellStyle}>{msToTimeIntervalString(aTrip.calculatedIdleDurationMs)}</TableRowColumn>
     <TableRowColumn style={cellStyle}>{metersToKmString(aTrip.calculatedDistanceM)}</TableRowColumn>
     <TableRowColumn style={cellStyle}>{speedToString(aTrip.maxSpeed)}</TableRowColumn>
-    <TableRowColumn style={cellStyle}>{speedToString(aTrip.avrSpeed)}</TableRowColumn>
+    <TableRowColumn style={cellStyle}>{speedToString(aTrip.avgSpeed)}</TableRowColumn>
     {aTrip.hasTemperature() &&
       <TableRowColumn style={cellStyle}>{temperatureToString(aTrip.maxTemp)}</TableRowColumn>
     }
@@ -60,54 +69,87 @@ const TripRow = ({
     }
   </TableRow>
 );
-
 TripRow.propTypes = {
   aTrip: React.PropTypes.object.isRequired,
 };
 
+const TotalRow = ({
+  reportFrame,
+}) => (
+  <TableRow className={styles.row} style={{ ...btmColorStyle, ...totalRowStyle }}>
+    <TableRowColumn style={cellStyle}>{msToTimeIntervalString(reportFrame.durationMs)}</TableRowColumn>
+    <TableRowColumn style={cellStyle}>-</TableRowColumn>
+    <TableRowColumn style={nameCellStyle}>-</TableRowColumn>
+    <TableRowColumn style={cellStyle}>-</TableRowColumn>
+    <TableRowColumn style={nameCellStyle}>-</TableRowColumn>
+    <TableRowColumn style={cellStyle}>{msToTimeIntervalString(reportFrame.totalOperatinMs)}</TableRowColumn>
+    <TableRowColumn style={cellStyle}>{msToTimeIntervalString(reportFrame.totalIdleMs)}</TableRowColumn>
+    <TableRowColumn style={cellStyle}>{metersToKmString(reportFrame.totalDistanceM)}</TableRowColumn>
+    <TableRowColumn style={cellStyle}>{speedToString(reportFrame.totalMaxSpeed)}</TableRowColumn>
+    <TableRowColumn style={cellStyle}>{speedToString(reportFrame.totalAvgSpeed)}</TableRowColumn>
+  </TableRow>
+);
 
-const UglyTable = ({
-  vehicleId,
-  getSoloReportById,
-}) => {
-  const reportFrame = getSoloReportById(vehicleId);
-  if (reportFrame === null) {
-    return false;
-  }
-  const trips = reportFrame.getValidTrips().map(aTrip => <TripRow key={aTrip.startIdx} aTrip={aTrip} />);
-  const hasT = reportFrame.getValidTrips()[0].hasTemperature();
-  const noBgdStyle = { backgroundColor: 'transparent' };
-  return (
-    <Layout.Content style={{ alignItems: 'center' }}>
-      <Table selectable={false} style={noBgdStyle}>
-        <TableHeader enableSelectAll={false} displaySelectAll={false} adjustForCheckbox={false} style={noBgdStyle}>
-          <TableRow style={btmColorStyle}>
-            <TableHeaderColumn style={cellStyle}>Start</TableHeaderColumn>
-            <TableHeaderColumn style={nameCellStyle}>From</TableHeaderColumn>
-            <TableHeaderColumn style={cellStyle}>End</TableHeaderColumn>
-            <TableHeaderColumn style={nameCellStyle}>To</TableHeaderColumn>
-            <TableHeaderColumn style={cellStyle}>Duration</TableHeaderColumn>
-            <TableHeaderColumn style={cellStyle}>Dist</TableHeaderColumn>
-            <TableHeaderColumn style={cellStyle}>MaxV</TableHeaderColumn>
-            <TableHeaderColumn style={cellStyle}>AvgV</TableHeaderColumn>
-            {hasT &&
-              <TableHeaderColumn style={cellStyle}>MaxT</TableHeaderColumn>
-            }
-            {hasT &&
-              <TableHeaderColumn style={cellStyle}>MinT</TableHeaderColumn>
-            }
-          </TableRow>
-        </TableHeader>
-        <TableBody style={noBgdStyle}>
-          {trips}
-        </TableBody>
-      </Table>
-      {/* <table style={{ border: 'solid 1px #aaa' }}>
-        {trips}
-      </table>*/}
-    </Layout.Content>
-  );
+TotalRow.propTypes = {
+  reportFrame: React.PropTypes.object.isRequired,
 };
+
+
+class UglyTable extends React.Component {
+  doPrint = () => {
+    // generatReportPDF(this.props.getSoloReportById(this.props.vehicleId));
+    generatReportPDF(this.tableRef);
+//    window.print();
+  }
+
+  render() {
+    const reportFrame = this.props.getSoloReportById(this.props.vehicleId);
+    if (reportFrame === null) {
+      return false;
+    }
+    const trips = reportFrame.getValidTrips().map(aTrip => <TripRow key={aTrip.startIdx} aTrip={aTrip} />);
+  // trips.push(<TotalRow key={reportFrame.posData.length} reportFrame={reportFrame} />);
+    const hasT = reportFrame.getValidTrips()[0].hasTemperature();
+    const noBgdStyle = { backgroundColor: 'transparent' };
+    return (
+      <Layout.Content style={{ alignItems: 'center' }}>
+        <Table ref={(input) => { this.tableRef = input; }} selectable={false} style={noBgdStyle}>
+          <TableHeader enableSelectAll={false} displaySelectAll={false} adjustForCheckbox={false} style={noBgdStyle}>
+            <TableRow style={btmColorStyle}>
+              <TableHeaderColumn style={cellStyle}>Rest Duration</TableHeaderColumn>
+              <TableHeaderColumn style={cellStyle}>Start</TableHeaderColumn>
+              <TableHeaderColumn style={nameCellStyle}>From</TableHeaderColumn>
+              <TableHeaderColumn style={cellStyle}>End</TableHeaderColumn>
+              <TableHeaderColumn style={nameCellStyle}>To</TableHeaderColumn>
+              <TableHeaderColumn style={cellStyle}>Operation Duration</TableHeaderColumn>
+              <TableHeaderColumn style={cellStyle}>Idle</TableHeaderColumn>
+              <TableHeaderColumn style={cellStyle}>Dist</TableHeaderColumn>
+              <TableHeaderColumn style={cellStyle}>MaxV</TableHeaderColumn>
+              <TableHeaderColumn style={cellStyle}>AvgV</TableHeaderColumn>
+              {hasT &&
+                <TableHeaderColumn style={cellStyle}>MaxT</TableHeaderColumn>
+              }
+              {hasT &&
+                <TableHeaderColumn style={cellStyle}>MinT</TableHeaderColumn>
+              }
+            </TableRow>
+          </TableHeader>
+          <TableBody style={noBgdStyle}>
+            {trips}
+          </TableBody>
+        </Table>
+        <div style={{ margin: 32 }}>
+          <MainActionButton
+            label={'PRINT'}
+            onClick={this.doPrint}
+            icon={null}
+          />
+        </div>
+      </Layout.Content>
+    );
+  }
+}
+
 
 UglyTable.propTypes = {
   vehicleId: React.PropTypes.string.isRequired,
