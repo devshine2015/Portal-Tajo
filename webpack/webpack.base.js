df9cf2e7d7ed180a371792128d89ec5f35870ee3
nikita <nikita.afanasev2020@gmail.com>
2017-07-10 16:04:28 +0200

    restoring web pack

--- webpack/webpack.base.js
+++ webpack/webpack.base.js
@@ -23,9 +23,6 @@ module.exports = (options) => ({
     publicPath: '/',
     sourceMapFilename: 'js/[name].js.map',
   }, options.output), // Merge with env dependent settings
-  node: {
-    fs: 'empty',
-  },
   module: {
     rules: [{
       test: /\.js$|\.jsx$/, // Transform all .js files required somewhere with Babel
@@ -72,9 +69,6 @@ module.exports = (options) => ({
     }, {
       test: /\.svg$/,
       loader: 'svg-react-loader',
-    }, {
-      test: /\.(png|woff|woff2|eot|ttf|svg)$/, 
-      loader: 'url-loader?limit=100000',
     }],
   },
   plugins: options.plugins.concat([
