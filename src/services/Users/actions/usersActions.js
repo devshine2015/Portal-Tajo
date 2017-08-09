410e9d3cd9081489a00887979364bf4d6d4cc875
nikita <nikita.afanasev2020@gmail.com>
2017-08-09 18:36:24 +0700

    fix(UsersManager): properly check if user has ability to see users list

--- src/services/Users/actions/usersActions.js
+++ src/services/Users/actions/usersActions.js
@@ -1,11 +1,11 @@
 import { auth0Api } from 'utils/api';
-import { auth } from 'utils/auth';
+import { authorizeWithRole } from 'utils/authz';
 import endpoints from 'configs/endpoints';
+import { getFleetName } from 'services/Session/reducer';
 import {
   assignRole,
   unassignRole,
 } from './rolesActions';
-import { getFleetName } from 'services/Session/reducer';
 
 export const USERS_MANAGER_USERS_SET = 'portal/UsersManager/USERS_MANAGER_USERS_SET';
 export const USERS_MANAGER_USER_CREATED = 'portal/UsersManager/USERS_MANAGER_USER_CREATED';
@@ -15,8 +15,8 @@ export const USERS_MANAGER_PERMISSION_UNASSIGN = 'portal/UsersManager/USERS_MANA
 export const USERS_MANAGER_USER_UPDATED = 'portal/UsersManager/USERS_MANAGER_USER_UPDATED';
 export const USERS_MANAGER_ROLES_SET = 'portal/UsersManager/USERS_MANAGER_ROLES_SET';
 
-const filterUsers = currentFleet => users => {
-  const allowedToSeeAllUsers = auth.authorizeWithRole('uber');
+const filterUsers = currentFleet => (users) => {
+  const allowedToSeeAllUsers = authorizeWithRole('uber');
 
   if (allowedToSeeAllUsers) return users;
 
@@ -38,8 +38,8 @@ export const fetchUsers = () => (dispatch, getState) => {
 export const createUser = ({
   fleet,
   role,
-  ...rest,
-}) => dispatch => {
+  ...rest, // eslint-disable-line
+}) => (dispatch) => {
   const { url, method, extName } = endpoints.createUser;
   const enrichedPayload = Object.assign({}, rest, {
     connection: 'Username-Password-Authentication',
@@ -51,7 +51,7 @@ export const createUser = ({
 
   return auth0Api[method](url, { payload: enrichedPayload, extName })
     .then(toJson)
-    .then(user => {
+    .then((user) => {
       dispatch(_userCreated(user));
       dispatch(assignRole(user.user_id, role));
 
@@ -59,11 +59,11 @@ export const createUser = ({
     });
 };
 
-export const deleteUser = userId => dispatch => {
+export const deleteUser = userId => (dispatch) => {
   const { url, method, extName } = endpoints.deleteUser(userId);
 
   return auth0Api[method](url, { extName })
-    .then(res => {
+    .then((res) => {
       if (res.status === 204) {
         dispatch(_userDeleted(userId));
         dispatch(unassignRole(userId));
@@ -92,27 +92,27 @@ function _updateUserCall(userId, payload) {
     .then(toJson);
 }
 
-export const changeEmail = (userId, payload) => dispatch => {
+export const changeEmail = (userId, payload) => (dispatch) => {
   const enrichedPayload = Object.assign({}, payload, {
     connection: 'Username-Password-Authentication',
     client_id: 'qlvnewPDcVdLge4ah7Rkp0lL9Lzikj7B',
   });
 
   return _updateUserCall(userId, enrichedPayload)
-    .then(user => {
+    .then((user) => {
       dispatch(_userUpdated(user, userId));
 
       return Promise.resolve();
     });
 };
 
-export const changePassword = (userId, payload) => dispatch => {
+export const changePassword = (userId, payload) => (dispatch) => {
   const enrichedPayload = Object.assign({}, payload, {
     connection: 'Username-Password-Authentication',
   });
 
   return _updateUserCall(userId, enrichedPayload)
-    .then(user => {
+    .then((user) => {
       dispatch(_userUpdated(user, userId));
 
       return Promise.resolve();
