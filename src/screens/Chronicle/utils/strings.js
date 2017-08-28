287033b2b720476fc58b1391e61b5e77a9d9c071
nikita <nikita.afanasev2020@gmail.com>
2017-08-28 12:09:41 +0200

    using text labels, depending on NoIcons flag

--- src/screens/Chronicle/utils/strings.js
+++ src/screens/Chronicle/utils/strings.js
@@ -1,3 +1,4 @@
+import { isNoIcons } from 'configs';
 import { metersToKmString, msToTimeIntervalString } from 'utils/convertors';
 import { makeStaticLableSVG, deviceAccessTime, localGasStation,
   notificationTimeToLeave, imageTimelapse, placesAcUnit } from './staticIcons';
@@ -26,10 +27,16 @@ export function textLable(lable, value, needsBr = true) {
 }
 
 export function dateToChronicleLable(inDate) {
+  if (isNoIcons) {
+    return `Time: <span style="float:right">${dateToChronicleStringTime(inDate)}</span>`;
+  }
   return `${makeStaticLableSVG(deviceAccessTime)}<span style="float:right">${dateToChronicleStringTime(inDate)}</span>`;
 }
 
 export function msToDurtationLable(duration) {
+  if (isNoIcons) {
+    return `Duration: <span style="float:right">${msToTimeIntervalString(duration)}</span>`;
+  }
   return `${makeStaticLableSVG(imageTimelapse)}<span style="float:right">${msToTimeIntervalString(duration)}</span>`;
 }
 
@@ -37,18 +44,30 @@ export function metersToDistanceLable(meters) {
   // Math.round((meters/1000) * 10) / 10
   // Math.round((meters/100) ) / 10
   // meters to km, rounding to 1 dec point
+  if (isNoIcons) {
+    return `Dist: <span style="float:right">${metersToKmString(meters)}</span>`;
+  }
   return `${makeStaticLableSVG(notificationTimeToLeave)}<span style="float:right">${metersToKmString(meters)}</span>`;
 }
 
 export function speedToChronicleLable(speed) {
+  if (isNoIcons) {
+    return `Speed: <span style="float:right">${speed.toFixed(1)} km/h</span>`;
+  }
   return `${makeStaticLableSVG(notificationTimeToLeave)}<span style="float:right">${speed.toFixed(1)} km/h</span>`;
 }
 
 export function temperatureToChronicleLable(temp) {
+  if (isNoIcons) {
+    return `Temp: <span style="float:right">${temp.toFixed(1)} &deg;C</span>`;
+  }
   return `${makeStaticLableSVG(placesAcUnit)}<span style="float:right">${temp.toFixed(1)} &deg;C</span>`;
 }
 
 export function fuelToChronicleLable(fuelNormalized) {
+  if (isNoIcons) {
+    return `Fuel: <span style="float:right">${(fuelNormalized * 100).toFixed(0)}%</span>`;
+  }
   return `${makeStaticLableSVG(localGasStation)}<span style="float:right">${(fuelNormalized * 100).toFixed(0)}%</span>`;
 }
 
