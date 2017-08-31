103afbeaace0d2f2c155c6ab6506902e2a9e2c57
nikita <nikita.afanasev2020@gmail.com>
2017-08-31 17:34:38 +0700

    fix(merging): fix merging bugs

--- src/screens/LoginScreen/components/LoginForm/LoginForm.js
+++ src/screens/LoginScreen/components/LoginForm/LoginForm.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import R from 'ramda';
 import { css } from 'aphrodite/no-important';
--- src/services/AlertsSystem/reducers/logReducer.js
+++ src/services/AlertsSystem/reducers/logReducer.js
@@ -25,9 +25,6 @@ export default function (state = initialState, action) {
     case SESSION_CLEAN:
       return initialState;
 
-    case cleanSession:
-      return initialState;
-
     default:
       return state;
   }
