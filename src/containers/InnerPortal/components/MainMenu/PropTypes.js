bdee4a19466af57ec5290c4db9567f2564571651
nikita <nikita.afanasev2020@gmail.com>
2017-09-07 14:04:36 +0700

    refactor(MainMenu): phrases shape updated

--- src/containers/InnerPortal/components/MainMenu/PropTypes.js
+++ src/containers/InnerPortal/components/MainMenu/PropTypes.js
@@ -1,9 +1,4 @@
-import PropTypes from 'prop-types';
-
-const shape = PropTypes.shape;
-const string = PropTypes.string;
-
-const phrases = [
+export default [
   'reports',
   'exec_report',
   'vehicles_editor',
@@ -19,22 +14,3 @@ const phrases = [
   'alerts_editor',
   'alerts_logs',
 ];
-
-export const phrasesShape = shape({
-  reports: string.isRequired,
-  exec_report: string.isRequired,
-  vehicles_editor: string.isRequired,
-  users: string.isRequired,
-  operational: string.isRequired,
-  profile: string.isRequired,
-  dashboard: string.isRequired,
-  installer: string.isRequired,
-  promos: string.isRequired,
-  devices_manager: string.isRequired,
-  review: string.isRequired,
-  history: string.isRequired,
-  alerts_editor: string.isRequired,
-  alerts_logs: string.isRequired,
-});
-
-export default phrases;
