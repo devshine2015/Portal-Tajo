6645c1b4e87b34a3800bc6986273f48082dd0588
nikita <nikita.afanasev2020@gmail.com>
2017-09-15 14:16:14 +0700

    refactor(utils): clean up utils index file and unused scripts

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,5 +2,3 @@ export { default as reporter } from './reports';
 export { default as classNameModifiers } from './classNameModifiers';
 export * as constants from './constants';
 export { default as cookies } from './cookies';
-export * as forms from './forms';
-export { default as storage } from './localStorage';
