5c1e23dcdd3941fcdb0665428ecc6885dca5c543
nikita <nikita.afanasev2020@gmail.com>
2017-09-15 10:19:02 +0700

    feat(mwa): redirect from `/mwa` to `login`. No fleet-specific routes
    We want to have single same root for customer portal. Explicit redirect ensures experience of
    existing users using those specific urls won't be broken

--- src/screens/LoginScreen/route.js
+++ src/screens/LoginScreen/route.js
@@ -1,16 +1,16 @@
-const createRoute = ({
-  childRoutes,
-  auth,
-}) => ({
-  childRoutes,
-  auth,
+const mwaRoute = state => /mwa/.test(state.location.pathname);
+
+const createRoute = options => ({
   name: 'login',
-  path: '/login',
   getComponent: (location, cb) => {
-    require.ensure([], require => {
+    require.ensure([], (require) => {
       cb(null, require('./index').default);
     }, 'login');
   },
+  onEnter: (nextState, replace) => {
+    if (mwaRoute(nextState)) replace('login');
+  },
+  ...options,
 });
 
 export default createRoute;
