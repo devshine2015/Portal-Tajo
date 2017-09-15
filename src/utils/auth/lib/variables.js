9d42c695b0a8be7194e763c8e3c2fd87fa77fd92
nikita <nikita.afanasev2020@gmail.com>
2017-09-15 23:37:16 +0700

    feat(auth0 settings): client config depends on active backend environment

--- src/utils/auth/lib/variables.js
+++ src/utils/auth/lib/variables.js
@@ -1,12 +1,16 @@
-// export default {
-//   domain: 'drvr.auth0.com',
-//   clientId: 'qlvnewPDcVdLge4ah7Rkp0lL9Lzikj7B',
-//   callbackUrl: 'http://localhost:3000/login/callback',
-// };
+const PROD_CLIENT = {
+  domain: 'drvr.auth0.com',
+  clientId: 'qlvnewPDcVdLge4ah7Rkp0lL9Lzikj7B',
+  callbackUrl: 'http://localhost:3000/login/callback',
+};
 
 // thomas client
-export default {
+const DEV_CLIENT = {
   domain: 'thomas-drvr.eu.auth0.com',
   clientId: 'lDBTNr8S_ipznvzqgr1GR6MF7RPAASsV',
   callbackUrl: 'http://localhost:3000/login/callback',
 };
+
+export default function getClientConfig(isProd) {
+  return isProd ? PROD_CLIENT : DEV_CLIENT;
+}
