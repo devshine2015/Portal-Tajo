10af860137f4369956305476d1342d7461d33aee
nikita <nikita.afanasev2020@gmail.com>
2017-09-18 18:35:31 +0700

    build: add dealer project to build and deployment process. Clean up unused code (like $EXTRA_FOLDERs

--- ci/commonDeploy.sh
+++ ci/commonDeploy.sh
@@ -2,23 +2,18 @@
 
 ESCAPE='tajo'
 SUNSHINE='portal'
+DEALER='dealer'
+BACKUP_FOLDER='public_backup'
 HOST=$1
 PUBLIC=$2
-BACKUP_FOLDER='public_backup'
 
 # server env defines how to build sources
 # could be 'dev' or 'prod'
 SERVER_ENV=$3
-EXTRA_FOLDER=$4
-
-if [ ! -z $EXTRA_FOLDER ] ; then
-  TARGET_FOLDER="$PUBLIC/$EXTRA_FOLDER"
-else
-  TARGET_FOLDER=$PUBLIC
-fi
 
 ESCAPE_FOLDER="builds/$SERVER_ENV/$ESCAPE"
 SUNSHINE_FOLDER="builds/$SERVER_ENV/$SUNSHINE"
+DEALER_FOLDER="builds/$SERVER_ENV/$DEALER"
 
 echo "rebuild static sources..."
 npm run clean
@@ -28,40 +23,33 @@ echo $SERVER_ENV
 if [ $SERVER_ENV = "prod" ] ; then
   npm run build:escape
   npm run build:sunshine
+  npm run build:dealer
 else
   npm run build:escape-dev
   npm run build:sunshine-dev
+  npm run build:dealer-dev
 fi
 
-
-if [ ! -z $EXTRA_FOLDER ] ; then
-  echo "Extra folder $EXTRA_FOLDER detected"
-
-  if (ssh $HOST "[ -d $PUBLIC/$EXTRA_FOLDER ]"); then
-    echo "$EXTRA_FOLDER already exists - remove and create again..."
-    ssh $HOST "cd $PUBLIC && rm -r $EXTRA_FOLDER && mkdir $EXTRA_FOLDER"
-  else
-    echo "$EXTRA_FOLDER not exist - creating..."
-    ssh $HOST "cd $PUBLIC && mkdir $EXTRA_FOLDER"
-  fi
-
-else
-
-  if [ $SERVER_ENV = "prod" ] ; then
-    echo "Creating backup copy of current runnnig version..."
-    
-    # 0. cd to public folder
-    # 1. create backup folder if it not exist
-    # 2. remove content of backup folder
-    # 3. copy content from public folder to backup folder
-    ssh $HOST "cd $PUBLIC && mkdir -p ../$BACKUP_FOLDER && rm -rf ../$BACKUP_FOLDER/* && cp -r ./* ../$BACKUP_FOLDER"    
-  fi
-
-  echo "erase root escape and sunshine files..."
-  ssh $HOST "cd $PUBLIC && rm -r ./*"
+if [ $SERVER_ENV = "prod" ] ; then
+  echo "Creating backup copy of current runnnig version..."
+  
+  # 0. cd to public folder
+  # 1. create backup folder if it not exist
+  # 2. remove content of backup folder
+  # 3. copy content from public folder to backup folder
+  ssh $HOST "cd $PUBLIC && mkdir -p ../$BACKUP_FOLDER && rm -rf ../$BACKUP_FOLDER/* && cp -r ./* ../$BACKUP_FOLDER"
 fi
 
+echo "erase files ..."
+ssh $HOST "cd $PUBLIC && rm -r ./*"
 
 echo "copy static files to server..."
-scp -r $SUNSHINE_FOLDER/* $HOST:$TARGET_FOLDER
-scp -r $ESCAPE_FOLDER $HOST:$TARGET_FOLDER
+echo "to $SUNSHINE folder"
+scp -r $SUNSHINE_FOLDER/* $HOST:$PUBLIC
+echo "to $ESCAPE folder"
+scp -r $ESCAPE_FOLDER $HOST:$PUBLIC
+echo "to $DEALER folder"
+scp -r $DEALER_FOLDER $HOST:$PUBLIC
+
+echo "Rename dealer to cc ..."
+ssh $HOST "cd $PUBLIC && mv $DEALER/ cc/"
--- ci/deployToDev.sh
+++ ci/deployToDev.sh
@@ -2,8 +2,7 @@
 
 HOST='ddsdev@ddsdev.cloudapp.net'
 PUBLIC='~/engine/static-root/public'
-EXTRA_FOLDER=$1
 
 parent_path=$( cd "$(dirname "${BASH_SOURCE}")" ; pwd -P )
 
-sh $parent_path/commonDeploy.sh $HOST $PUBLIC 'dev' $EXTRA_FOLDER 
+sh $parent_path/commonDeploy.sh $HOST $PUBLIC 'dev'
