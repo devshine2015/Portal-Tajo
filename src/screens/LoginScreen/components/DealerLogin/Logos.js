69ec59e3ee5b67f0b9f516e53c13812d970367c1
nikita <nikita.afanasev2020@gmail.com>
2017-09-18 12:30:57 +0700

    refactor(Logos): use optimized picture of logos

--- src/screens/LoginScreen/components/DealerLogin/Logos.js
+++ src/screens/LoginScreen/components/DealerLogin/Logos.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { css } from 'aphrodite/no-important';
-import fuso from './images/fuso_logo.jpg';
-import cc from './images/cc_logo.png';
+import fuso from './images/fuso_logo_small.png';
+import cc from './images/cc_logo_small.png';
 import classes from './Logos.classes';
 
 const BrandLogos = () => {
