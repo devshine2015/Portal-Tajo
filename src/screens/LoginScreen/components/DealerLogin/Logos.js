cc7b7782a0c2433ea14de2b34fca48d1fc0ef0b6
nikita <nikita.afanasev2020@gmail.com>
2017-09-19 12:04:15 +0700

    refactor(assets): move cc assets from LoginScreen to common assets dir to be reusable

--- src/screens/LoginScreen/components/DealerLogin/Logos.js
+++ src/screens/LoginScreen/components/DealerLogin/Logos.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import { css } from 'aphrodite/no-important';
-import fuso from './images/fuso_logo_small.png';
-import cc from './images/cc_logo_small.png';
+import fuso from 'assets/images/logos/cc/fuso_logo.png';
+import cc from 'assets/images/logos/cc/cc_logo.png';
 import classes from './Logos.classes';
 
 const BrandLogos = () => {
   return (
     <div className={css(classes.logos)}>
-      <img src={fuso} className={css(classes.logos__logo)} alt="" />
-      <img src={cc} className={css(classes.logos__logo)} alt="" />
+      <img
+        src={fuso}
+        className={css(classes.logos__logo)}
+        alt=""
+      />
+      <img
+        src={cc}
+        className={css(classes.logos__logo)}
+        alt=""
+      />
     </div>
   );
 };
