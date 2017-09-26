13f1d575dc2bce1d26a2f6fcceaff84c177d73a8
nikita <nikita.afanasev2020@gmail.com>
2017-09-26 13:08:31 +0700

    feat(Users Manager): service provides selectors

---
+++ src/services/Users/selectors.js
@@ -0,0 +1,27 @@
+import globalizeSelectors from 'utils/globalizeSelectors';
+import { reducerKey } from './reducer';
+import { reducerKey as usersReducerKey } from './reducers/users';
+import { reducerKey as permissionsReducerKey } from './reducers/permissions';
+import { reducerKey as rolesReducerKey } from './reducers/roles';
+import { reducerKey as readyStateReducerKey } from './reducers/readyState';
+
+const usersLocalState = state => state.getIn([reducerKey, usersReducerKey]);
+const rolesLocalState = state => state.getIn([reducerKey, rolesReducerKey]);
+const permissionsLocalState = state => state.getIn([reducerKey, permissionsReducerKey]);
+const readyStateLocalState = state => state.getIn([reducerKey, readyStateReducerKey]);
+
+// local selector
+export const getUsers = state => state.get('usersList');
+export const getRoles = state => state.get('map');
+export const getReadyState = state => state.get('isReady');
+
+export const usersSelectors = globalizeSelectors(usersLocalState, {
+  getUsers,
+});
+export const rolesSelectors = globalizeSelectors(rolesLocalState, {
+  getRoles,
+});
+export const permissionsSelectors = globalizeSelectors(permissionsLocalState, {});
+export const readyStateSelectors = globalizeSelectors(readyStateLocalState, {
+  getReadyState,
+});
