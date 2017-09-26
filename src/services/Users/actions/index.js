292551a2603b9a2dc6a32800f94f119efe6c2462
nikita <nikita.afanasev2020@gmail.com>
2017-09-26 13:03:31 +0700

    feat(Users Manager): fetch users right after getting roles and permissions. Set service ready state

--- src/services/Users/actions/index.js
+++ src/services/Users/actions/index.js
@@ -6,7 +6,19 @@ export const usersActions = usrsActions;
 export const rolesActions = rlsActions;
 export const permissionsActions = prmsnsActions;
 
+export const USERS_MANAGER_READY_STATE_CHANGE = 'change ready state of Users Manager';
+
 export const fetchRolesAndPermissions = () => (dispatch) => {
+  dispatch(_changeReadyState('loading'));
+
   return dispatch(prmsnsActions.fetchPermissions())
-    .then(() => dispatch(rlsActions.fetchRoles()));
+    .then(() => dispatch(rlsActions.fetchRoles()))
+    .then(() => dispatch(usrsActions.fetchUsers()))
+    .then(() => dispatch(_changeReadyState('ready')))
+    .catch(() => dispatch(_changeReadyState('error')));
 };
+
+const _changeReadyState = nextState => ({
+  type: USERS_MANAGER_READY_STATE_CHANGE,
+  nextState,
+});
