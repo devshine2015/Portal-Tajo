9bdf13dd37e9cd54484b5c26d18a7229df5c6869
nikita <nikita.afanasev2020@gmail.com>
2017-10-10 10:41:31 +0700

    feat(ProfileScreen): any user can see and edit their profile

--- src/screens/Profile/index.js
+++ src/screens/Profile/index.js
@@ -1,21 +1,16 @@
 import React from 'react';
 import { css } from 'aphrodite/no-important';
-import { authorizeWithPermissions } from 'utils/authz';
 import Layout from 'components/Layout';
 import ProfileDetails from './components/ProfileDetails';
 import LanguageWidget from './components/LanguageWidget';
 
 import classes from './classes';
 
-const userCanSeeProfile = () => authorizeWithPermissions('view:profile');
-
 const ProfileScreen = () => (
   <Layout.Content>
-    { userCanSeeProfile() && (
-      <div className={css(classes.widget)}>
-        <ProfileDetails />
-      </div>
-    )}
+    <div className={css(classes.widget)}>
+      <ProfileDetails />
+    </div>
     <div className={css(classes.widget)}>
       <LanguageWidget />
     </div>
