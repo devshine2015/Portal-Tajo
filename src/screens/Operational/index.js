a5c3cad8d47cde9d62fd0319551dd57e6f9e9227
nikita <nikita.afanasev2020@gmail.com>
2017-10-24 17:00:07 +0700

    fix(operational): don't open new connection on mount if it already exist

--- src/screens/Operational/index.js
+++ src/screens/Operational/index.js
@@ -2,39 +2,33 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import pure from 'recompose/pure';
 import { connect } from 'react-redux';
-
 import Layout from 'components/Layout';
 import PowerList from 'components/PowerList';
 import TheMap from 'containers/Map/MapContainer';
-// import Journal from 'containers/Journal/components/Journal';
-import OperationalList from './components/OperationalPowerList';
 import FixedContent from 'components/FixedContent';
 import RouteFinder from 'containers/Map/OnMapElements/MapRoute/RouteFinder';
 import NearestFinder from 'containers/Map/OnMapElements/MapRoute/NearestFinder';
 import RoutePath from 'containers/Map/OnMapElements/MapRoute/RoutePath';
 import CtxtOpenGoogleMap from 'containers/Map/OnMapElements/CtxtMenuOpenGMap';
-
 import GFEditor from 'containers/GFEditor/GFEditor';
 import GFEditorMapComponent from 'containers/GFEditor/MapComponenet';
-
 import markerTypes from 'services/FleetModel/utils/markerTypes';
 import * as fromFleetReducer from 'services/FleetModel/reducer';
 import { socketActions, localTickActions } from 'services/FleetModel/actions';
 import { gfEditIsEditing } from 'containers/GFEditor/reducer';
-
 import { mapVehicleMarkerMaker } from 'containers/Map/OnMapElements/MapVehicle';
 import { mapVehicleNameMaker } from 'containers/Map/OnMapElements/VehicleNameMarker';
 import { mapGFMarkerMaker } from 'containers/Map/OnMapElements/MapGF';
 import { mapMWAJobMarkerMaker } from 'containers/Map/OnMapElements/MWAJobMarker';
-
 import { getMWAJobs } from 'services/MWA/reducer';
+import OperationalList from './components/OperationalPowerList';
+
 
 import styles from './styles.css';
 
 class Operational extends React.Component {
-
   componentDidMount() {
-    if (this.props.vehicles.length > 0) {
+    if (this.props.vehicles.length > 0 && !socketActions.isSocketOpened()) {
       this.props.openFleetSocket();
       // TODO: move it none layer higher -
       // keep local tick alife all the time - actiual in any screen
