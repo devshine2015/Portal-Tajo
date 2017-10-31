0fa0085e2229c6722d7c5097d464bd88c9253cd7
nikita <nikita.afanasev2020@gmail.com>
2017-10-31 07:17:58 +0100

    local alerts validator

---
+++ src/services/AlertsSystem/utils/localAlertsValidator.js
@@ -0,0 +1,19 @@
+const conditionsToCheck = {};
+
+const addLocalALertCondition = (alertCondition) => {
+
+}
+
+const checkVehicle = (vehicleObj, conditionId) => {
+
+}
+
+const _checkTempAler = (vehicleObj, alertConditionObj) => {
+  
+}
+
+const localAlertsHelper = {
+  addLocalALertCondition,
+};
+
+export default localAlertsHelper;
