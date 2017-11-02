a6eaf5a18e23ed76e4bd8b28ffda9887f1319ca9
nikita <nikita.afanasev2020@gmail.com>
2017-11-02 14:52:03 +0100

    fix(GridFleet selector sorted vehicles):

--- src/screens/CustomerDashboard/components/WindowedSummary/selectors.js
+++ src/screens/CustomerDashboard/components/WindowedSummary/selectors.js
@@ -5,19 +5,21 @@ import localAlertsProcessor from 'services/AlertsSystem/utils/localAlertsProcess
 import { sortByName } from 'utils/sorting';
 
 export default function makeGetVehicles() {
-  return createSelector(getProcessedVehicles, vehicles => vehicles.toArray()
-    .map(v => ({
-      id: v.get('id'),
-      name: v.getIn(['original', 'name']),
-      speed: v.get('speed').toFixed(0),
-      fuel: R.isNil(v.get('fuel')) ? undefined : v.get('fuel').toFixed(1),
-      temp: R.isNil(v.get('temp')) ? undefined : v.get('temp').toFixed(1),
-      pos: {
-        lat: v.get('pos')[0],
-        lng: v.get('pos')[1],
-      },
-      lastUpdate: v.get('lastUpdateSinceEpoch'),
-      alertsState: localAlertsProcessor.getVehicleAlertState(v.toJS()),
-    })))
-    .sort(sortByName);
+  return createSelector(getProcessedVehicles, vehicles =>
+    vehicles.toArray()
+      .map(v => ({
+        id: v.get('id'),
+        name: v.getIn(['original', 'name']),
+        speed: v.get('speed').toFixed(0),
+        fuel: R.isNil(v.get('fuel')) ? undefined : v.get('fuel').toFixed(1),
+        temp: R.isNil(v.get('temp')) ? undefined : v.get('temp').toFixed(1),
+        pos: {
+          lat: v.get('pos')[0],
+          lng: v.get('pos')[1],
+        },
+        lastUpdate: v.get('lastUpdateSinceEpoch'),
+        alertsState: localAlertsProcessor.getVehicleAlertState(v.toJS()),
+      }))
+      .sort(sortByName),
+  );
 }
