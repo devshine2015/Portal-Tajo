875234f07939672f700136f4dc969689a000cf24
nikita <nikita.afanasev2020@gmail.com>
2017-11-20 12:32:50 +0100

    Revert "sorted out double total running time header"
    This reverts commit ce71d12ac0a07c48a9b9df2cbfcde1dada6d292d.

--- src/screens/DealerDashboard/components/Page.js
+++ src/screens/DealerDashboard/components/Page.js
@@ -46,7 +46,7 @@ class DealerDashboard extends React.Component {
             </div>
             <div className="col-md-2" style={noPadding}>
               <DashboardElements.DataCard
-                title={'Total Distance Travelled KM'}
+                title={'Total Driving Time'}
                 dataString={'2750'}
               />
             </div>
