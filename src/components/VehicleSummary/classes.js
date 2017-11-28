4fce86cc3c5f9719a28f51e2fe7c99ec20fe1d4f
nikita <nikita.afanasev2020@gmail.com>
2017-11-28 11:10:11 +0100

    feat(vehicle summary misc):

--- src/components/VehicleSummary/classes.js
+++ src/components/VehicleSummary/classes.js
@@ -1,24 +1,29 @@
 import { StyleSheet } from 'aphrodite/no-important';
+import tinycolor from 'tinycolor2';
+import { theme } from 'configs';
 
 // const DEFAULT_WIDGET_WIDTH = 600;
+const fontColor = tinycolor(theme.palette.primary1Color).setAlpha(0.75).toString();
+// color: '#999',
 
 const classes = StyleSheet.create({
   title: {
     textAlign: 'center',
     paddingLeft: 0,
     fontSize: 24,
-    color: '#999',
     fontWeight: 'bold',
+    color: fontColor,
     // display: 'flex',
     // justifyContent: 'space-between',
     // alignItems: 'flex-end',
-  },  
+  },
   itemsContainer: {
     display: 'flex',
     flexWrap: 'wrap',
     // flex: 1,
     justifyContent: 'center',
     width: '100%',
+    color: fontColor,
   },
   itemBody: {
     display: 'flex',
@@ -29,7 +34,6 @@ const classes = StyleSheet.create({
   itemTitle: {
     paddingRight: '8px',
     fontSize: 12,
-    color: '#999',
     fontWeight: 200,
     marginBottom: 10,
     position: 'relative',
@@ -40,7 +44,6 @@ const classes = StyleSheet.create({
   },
   itemValue: {
     fontSize: 18,
-    color: '#999',
     fontWeight: 200,
     marginBottom: 10,
     // display: 'flex',
