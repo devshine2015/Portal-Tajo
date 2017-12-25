5da8c1351cf431f0970bc3bf6331a41c327cfdff
nikita <nikita.afanasev2020@gmail.com>
2017-12-25 13:49:55 +0100

    feat(date/time formatting):

--- src/screens/Chronicle/utils/strings.js
+++ src/screens/Chronicle/utils/strings.js
@@ -1,11 +1,12 @@
+import moment from 'moment';
 import { isNoIcons } from 'configs';
 import { metersToKmString, msToTimeIntervalString, fuelToString } from 'utils/convertors';
 import { makeStaticLableSVG, deviceAccessTime, localGasStation,
   notificationTimeToLeave, imageTimelapse, placesAcUnit } from './staticIcons';
-  
+
 //
 // need this for setting content of mapBox popUp
-export function generateInnerHTMLForHistoryMoment(momentData, theVehicle/* , phrases = {}*/) {
+export function generateInnerHTMLForHistoryMoment(momentData, theVehicle/* , phrases = {} */) {
   let content = `${dateToChronicleLable(momentData.date)}<br>
                  ${speedToChronicleLable(momentData.speed)}`;
 
@@ -15,9 +16,6 @@ export function generateInnerHTMLForHistoryMoment(momentData, theVehicle/* , phr
   if (momentData.fuel !== null) {
     content += `<br> ${fuelToChronicleLable(momentData.fuel, theVehicle.original.fuelCapacity)}`;
   }
-  // content += `<hr>${phrases.lat || 'lat'}:<span style="float:right">${momentData.pos.lat.toFixed(6)}</span>
-  //             <br>
-  //             ${phrases.lng || 'lng'}:<span style="float:right">${momentData.pos.lng.toFixed(6)}</span>`;
 
   return content;
 }
@@ -73,29 +71,9 @@ function fuelToChronicleLable(fuelNormalized, fuelCapacity) {
 }
 
 function dateToChronicleStringTime(inDate) {
-  const timeStr = `${(inDate.getHours() < 10 ? 0 : '')
-                + inDate.getHours()
-                 }:${
-                 inDate.getMinutes() < 10 ? 0 : ''
-                 }${inDate.getMinutes()
-                 }`;
-                //  :${
-                //  inDate.getSeconds() < 10 ? 0 : ''
-                //  }${inDate.getSeconds()}`;
-  return timeStr;
+  return moment(inDate).format('HH:mm');
 }
 
 export function dateToChronicleString(inDate) {
-  const timeStr = `${inDate.toLocaleDateString()}
-                  ${' '}
-                  ${(inDate.getHours() < 10 ? 0 : '')
-                  + inDate.getHours()
-                 }:${
-                 inDate.getMinutes() < 10 ? 0 : ''
-                 }${inDate.getMinutes()
-                 }`;
-                //  :${
-                //  inDate.getSeconds() < 10 ? 0 : ''
-                //  }${inDate.getSeconds()}`;
-  return timeStr;
+  return moment(inDate).format('DD-MM-YY HH:mm');
 }
