fc9267c90f043db5e9bbc34e089ccc57ce52adc0
nikita <nikita.afanasev2020@gmail.com>
2018-04-11 16:50:57 +0300

    add clickableRow, comment out location column, add lat/lng props

--- src/screens/FuelUsage/components/FuelAlerts.js
+++ src/screens/FuelUsage/components/FuelAlerts.js
@@ -14,27 +14,55 @@ import {
 } from 'material-ui/Table';
 import inClasses from './classes';
 
+const ClickableRow = (props) => {
+  // destructure props to keep the expected MUI TableRow props
+  // while having access to the rowData prop
+  const { lat, lng, ...restProps } = props;
+  return (
+    <TableRow
+      {...restProps}
+      onMouseDown={() => console.log('clicked', props)}
+    >
+      {props.children}
+    </TableRow>
+  );
+};
+
 const FuelConsumption = ({ vehicleAlerts, totalConsumption, height }) => {
   const tableData = vehicleAlerts.map(
-    alert => (
-      <TableRow key={alert.date}>
+    (alert, i) => (
+      <ClickableRow
+        key={`${moment(alert.date).utc().format('YYYY-MM-DDTHH-mm-ss-SSS')}&${i}`}
+        lat={alert.position.lat}
+        lng={alert.position.lng}
+      >
         <TableRowColumn>{alert.alertType}</TableRowColumn>
         <TableRowColumn>{moment(alert.date).utc().format('DD/MM/YY HH:mm')}</TableRowColumn>
-        <TableRowColumn>{alert.position ? `${alert.position.lat.toFixed(3)}, ${alert.position.lng.toFixed(3)}` : 'N/A'}</TableRowColumn>
+        {/* <TableRowColumn>{alert.position ?
+          `${alert.position.lat.toFixed(3)}, ${alert.position.lng.toFixed(3)}` :
+          'N/A'}</TableRowColumn> */}
         <TableRowColumn>{alert.liters.toFixed(1).toString()}</TableRowColumn>
-        <TableRowColumn> {totalConsumption > 0 ? (100 * alert.liters / totalConsumption).toFixed(1).toString() : 'N/A'} </TableRowColumn>
-      </TableRow>
+        <TableRowColumn> {totalConsumption > 0 ?
+          ((100 * alert.liters) / totalConsumption).toFixed(1).toString() :
+          'N/A'} </TableRowColumn>
+      </ClickableRow>
     ));
 
   return (
     <div className={css(inClasses.container)}>
       <div className={css(inClasses.containerHeading)}>Fuel Alerts</div>
-      <Table height={height}>
-        <TableHeader displaySelectAll={false} adjustForCheckbox={false}>
+      <Table
+        height={height}
+        selectable={false}
+      >
+        <TableHeader
+          adjustForCheckbox={false}
+          displaySelectAll={false}
+        >
           <TableRow>
             <TableHeaderColumn>Alert Type</TableHeaderColumn>
             <TableHeaderColumn>Date/Time</TableHeaderColumn>
-            <TableHeaderColumn>Location</TableHeaderColumn>
+            {/* <TableHeaderColumn>Location</TableHeaderColumn> */}
             <TableHeaderColumn>Liters</TableHeaderColumn>
             <TableHeaderColumn>% of consumption</TableHeaderColumn>
           </TableRow>
