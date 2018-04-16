dd4b65fb79ab8de5a9525e03f2e7cb2a0c1cbdb2
nikita <nikita.afanasev2020@gmail.com>
2018-04-16 12:48:53 +0300

    add 'redux-devtools-extension', remove 'redux-logger'

--- src/configs/store.js
+++ src/configs/store.js
@@ -2,42 +2,29 @@
  * Create the store with asynchronously loaded reducers
  */
 
-import { createStore, applyMiddleware, compose } from 'redux';
-import { fromJS } from 'immutable';
-import thunk from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux';
 import { routerMiddleware } from 'react-router-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import { fromJS } from 'immutable';
 import { api, auth0Api } from 'utils/api';
 import { portal } from './index';
 
-const devtools = window.devToolsExtension || (() => noop => noop);
-const isDev = process.env.NODE_ENV !== 'production';
 
 export default function configureStore(initialState = {}, history, createReducer) {
-  const enhancers = [
-    devtools(),
-  ];
-
-  const middlewares = [
+  const middleware = [
     thunk,
     routerMiddleware(history),
   ];
 
-  if (isDev) {
-    const createLogger = require('redux-logger').createLogger; // eslint-disable-line import/no-extraneous-dependencies
-    const logger = createLogger({
-      predicate: isDev,
-      collapsed: true,
-      duration: true,
-    });
-    middlewares.push(logger);
-  }
-
-  const createAppStore = applyMiddleware(...middlewares)(createStore);
-  const store = createAppStore(
+  const store = createStore(
     createReducer(),
-    fromJS(initialState),
-    compose(...enhancers),
+    fromJS(initialState), // preloadedState
+    composeWithDevTools(
+      applyMiddleware(...middleware),
+    ),
   );
+
 
   // Extensions
   store.asyncReducers = {}; // Async reducer registry
